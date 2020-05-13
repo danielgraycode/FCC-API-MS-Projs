a3cad196aa5fef19cd8222f5523ac63a622fae01
Daniel Gray <me@danielgray.email>
2020-05-13 19:57:36 +0100

    Complete the URL shortener project

--- url-shortener-proj/server.js
+++ url-shortener-proj/server.js
@@ -1,37 +1,77 @@
-'use strict';
+"use strict";
+require("dotenv").config();
+var express = require("express");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var Schema = mongoose.Schema;
+var crypto = require("crypto");
+var dns = require("dns");
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-
-var cors = require('cors');
+var cors = require("cors");
 
 var app = express();
 
-// Basic Configuration 
-var port = process.env.PORT || 3000;
+// Basic Configuration
+var port = 8765;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.DB_URI);
+mongoose.connect(process.env.DB_URI);
+var urlSchema = new Schema({
+  original_url: String,
+  short_url: String,
+});
+var URL = mongoose.model("URL", urlSchema);
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.post("/api/shorturl/new", function (req, res) {
+  //Check the URL is valid
+  let hostname = req.body.url.split("/");
+  dns.lookup(hostname[2], function (error) {
+    //If hostname is invalid
+    if (error) {
+      res.json({ error: "Invalid URL" });
+    } else {
+      //Make the new url object
+      let newurl = new URL({
+        original_url: req.body.url,
+        short_url: crypto.randomBytes(8).toString("hex"),
+      });
+      //Save the new url to the database
+      newurl.save(function (error, data) {
+        if (error) return console.log(error);
+        //Show the user the url they shortened and the short url path
+        res.json({
+          original_url: data.original_url,
+          short_url: data.short_url,
+        });
+      });
+    }
+  });
 });
 
+app.get("/api/shorturl/:shorturl*", function (req, res) {
+  //Get the short url
+  let short_url = req.params.shorturl;
+  //Query database
+  URL.find({ short_url: short_url }, function (error, data) {
+    if (error) return console.erorr(error);
+    //If URL found redirect, otherwise return an error
+    if (data) {
+      res.redirect(data[0].original_url);
+    } else {
+      res.status(404).json({ error: "Invalid URL" });
+    }
+  });
+});
 
 app.listen(port, function () {
-  console.log('Node.js listening ...');
+  console.log("Node.js listening ...");
 });
